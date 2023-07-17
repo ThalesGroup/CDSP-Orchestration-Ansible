65b56fae429ef6bc3bae2fab80b93db6c6dbea99
anugram <anurag.jain@thalesgroup.com>
2023-07-17 11:26:27 -0400

    Update License info in each file

--- plugins/modules/licensing_lockdata_get.py
+++ plugins/modules/licensing_lockdata_get.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# (c) 2022 Thales Group. All rights reserved.
+# (c) 2023 Thales Group. All rights reserved.
+# Author: Anurag Jain, Developer Advocate, Thales
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#  http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Licensed under the MIT License
 #
 
 from __future__ import absolute_import, division, print_function
